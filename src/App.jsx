26f58841379618c44d034284650fa053e2832d95
jhyuk <jh.jang@outlook.com>
2023-11-30 10:40:05 +0900

    use-animate

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from 'react';
+import A from './components/A';
+import B from './components/B';
+
+const App = () => {
+  const [isObserve, setIsObserve] = useState(false);
+
+  const bRef = useRef();
+  const observer = useRef();
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        setIsObserve(entries[0].isIntersecting);
+      },
+      { threshold: 0.5 }
+    );
+
+    observer.current.observe(bRef.current);
+
+    return () => observer.current.unobserve(bRef.current);
+  }, []);
+
+  return (
+    <>
+      <A />
+      <B bRef={bRef} isObserve={isObserve} />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/A.jsx
@@ -0,0 +1,13 @@
+const A = () => {
+  return (
+    <div className='bg-white min-h-screen flex justify-center items-center '>
+      <img
+        className='img-cover w-4/12 animate__animated animate__rollIn animate__infinite	infinite animate__slower'
+        src='./images/cs.jpg'
+        alt='jungle'
+      />
+    </div>
+  );
+};
+
+export default A;
