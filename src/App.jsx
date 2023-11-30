0ed461d2ab3d5340104de88c4e17fdc9959bb9d5
jhyuk <jh.jang@outlook.com>
2023-11-30 11:42:25 +0900

    custom-hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import A from './components/A';
 import B from './components/B';
 
 const App = () => {
-  const [isObserve, setIsObserve] = useState(false);
-
-  const bRef = useRef();
-  const observer = useRef();
-
-  useEffect(() => {
-    observer.current = new IntersectionObserver(
-      (entries) => {
-        setIsObserve(entries[0].isIntersecting);
-      },
-      { threshold: 0.5 }
-    );
-
-    observer.current.observe(bRef.current);
-
-    return () => observer.current.unobserve(bRef.current);
-  }, []);
-
   return (
     <>
       <A />
-      <B bRef={bRef} isObserve={isObserve} />
+      <B />
     </>
   );
 };
